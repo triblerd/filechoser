59f94cc2f103fb05127a76730b140b2488961a4a
S.Pan <np.sandip.pandey@gmail.com>
2023-06-12 15:42:44 +0200

    Specify pyinstaller from venv bin

--- build.sh
+++ build.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-if [ ! -z "$VENV" ]; then
-  echo "Setting venv to $VENV"
-  source $VENV/bin/activate
-else
-  echo "Creating a new venv"
-  python3 -m venv build-env
-  . ./build-env/bin/activate
-fi
+
+echo "Creating a new venv"
+python3 -m venv build-env
+. ./build-env/bin/activate
 
 # ----- Install dependencies before the build
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade -r requirements.txt
 
 # Create the binary
-python3 -m pyinstaller -v
-python3 -m pyinstaller --onefile --windowed main.py
+./build-env/bin/pyinstaller -v
+./build-env/bin/pyinstaller --onefile --windowed main.py
 
 # deactivate the virtual env at the end
 deactivate
